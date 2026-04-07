99a2742a7af53f1099fde8d8cb3d879264b0886d
faishalsaifi <faishalsaifi700@gmail.com>
2026-04-07 11:02:38 +0530

    fix

--- models/db.js
+++ models/db.js
@@ -8,6 +8,8 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+   connectTimeout: 30000,
+  ssl: { rejectUnauthorized: false }
 
 });
 
