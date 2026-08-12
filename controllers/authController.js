d4640b30a5e709686e9326fda6c5dcbcb4b6da19
faishalsaifi <faishalsaifi700@gmail.com>
2026-08-12 14:23:30 +0530

    fix 1st attempt

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,14 +37,16 @@ exports.sendOtpForSignup = async (req, res) => {
 
     // 📧 Send OTP via email
     const transporter = nodemailer.createTransport({
-       host: "smtp.gmail.com",
-  port: 465,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
       }
     });
-
+await transporter.verify();
+console.log("✅ Gmail SMTP connection successful");
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
