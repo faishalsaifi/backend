ca0ccd51ba118d6458ea10a0a376ca983004e2c7
faishalsaifi <faishalsaifi700@gmail.com>
2026-08-12 13:57:11 +0530

    minor fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,8 @@ exports.sendOtpForSignup = async (req, res) => {
 
     // 📧 Send OTP via email
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+       host: "smtp.gmail.com",
+  port: 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
